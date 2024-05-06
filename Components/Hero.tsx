5cf0d6ee5a1b9a805044646333c292179d9d54b2
Eshrat <hasan.techtomorrow@gmail.com>
2024-05-06 13:13:21 +0600

    first commit

---
+++ Components/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='flex-1 pt-36 padding-x'>
+                <h1 className='hero__title'>
+                    Find, Book, Or Rent a Car - quick and super easy !
+                </h1>
+                <p className='hero__subtitle'>
+                    Streamline your car rental experience with our effortless booking process.
+                </p>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
